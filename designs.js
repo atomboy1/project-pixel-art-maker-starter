253aab07a3e4e9d482301b2697507848ae4268eb
atomboy1 <39170879+atomboy1@users.noreply.github.com>
2018-05-24 17:58:27 +0100

    Update designs.js
    check it out

--- designs.js
+++ designs.js
@@ -1,41 +1,45 @@
-/**
-* @declare variables
-*/
-var row;
-var column;
-var color;
+// Select color input
+const color = document.querySelector('#colorPicker');
+// Select size input
+const height = document.querySelector('#inputHeight');
+const width = document.querySelector('#inputWidth');
+const sizePicker = document.querySelector('#sizePicker');
+const canvas = document.querySelector('#pixelCanvas');
 
-/**
-* @ desciption - Call makeGrid function on form submit
-* @ .preventDefault() - Cancels the default action (navigation) of the form submission
-*/
-$("#sizePicker").submit(function(event){
-    event.preventDefault();
-    row = $("#inputHeight").val();
-    column = $("#inputWidth").val();
-    makeGrid(row,column);
-  })
-/**
-* @desciption - make dynamic grid with user entered values
-* @ .remove() - removes all table rows and table data
-* @ .append() - inserts rows to last child of the specified element
-*/
-function makeGrid(r,c){
-    $("tr").remove();
-    var i=1;
-    while(i<=r){
-      $("#pixelCanvas").append("<tr id='table"+ i +"'></tr>");
-      for(var j=1;j<=c;j++){
-        $("#table" + i).append("<td></td>");
-    }i++;
-  }
-/**
-* @desciption - fills/removes color of the selected grid cell
-* @ .attr() - gets value of the element attribute matched to "style"
-* @ .removeAttr() - removes value of the element attribute matched to "style"
-*/
-$("td").click(function addColor(){
-    color = $("#colorPicker").val();
-    $(this).attr("style","background-color:" + color);
-  })
+
+function clearOldGrid() {
+// clear the old grid first
+canvas.innerHTML = '';
 }
+
+function makeGrid() {
+let tr, td;
+//create table: (for loop runs from inside, then outside). create td, append to tr; create tr, tr to table(canvas).
+for (let i = 0; i < height.value; i++) {
+    tr = document.createElement('tr');
+    canvas.appendChild(tr);
+    for (let j = 0; j < width.value; j++) {
+        td = document.createElement('td');
+        tr.appendChild(td);
+    }
+}
+}
+
+function changeCellColor() {
+//check which TD is clicked, change its color (Udacity Lesson 21: 5. Avoid Too Many Events talks about this)
+canvas.addEventListener('click', function(e) {
+    e.preventDefault();
+    // check using capital letters
+    if (e.target.nodeName === 'TD') {
+        e.target.style.backgroundColor = color.value;
+    }
+});
+}
+
+// When size is submitted by the user, call makeGrid()
+sizePicker.addEventListener('submit', function(e) {
+e.preventDefault();
+clearOldGrid();
+makeGrid();
+changeCellColor();
+});
